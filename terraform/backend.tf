b1197e3dfed054ba7af90b14e8295ab11e907ef7
Nour <aann20034@gmail.com>
2026-05-10 22:33:57 +0100

    Dynamo db state locking is deprecated so switching to lockfile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "eks-sre-platform-terraform-state"
     key            = "platform/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "eks-sre-platform-terraform-locks"
     encrypt        = true
+    use_lockfile   = true
   }
 }
