5df77b94607c9a93355c3acc0d600945696d7e4a
Nour <aann20034@gmail.com>
2026-05-13 15:23:22 +0100

    Improved hpa test script

--- scripts/hpa-test.sh
+++ scripts/hpa-test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-kubectl get hpa -n sre-demo-app -w
+echo "Watching HPA and app pods in sre-demo-app namespace..."
+echo "Press CTRL+C to stop."
+
+watch -n 2 '
+echo "=== HPA ==="
+kubectl get hpa -n sre-demo-app
+echo ""
+echo "=== Pods ==="
+kubectl get pods -n sre-demo-app
+'
