0848a387d18837b70497aed7304089ade3310ad6
Nour <aann20034@gmail.com>
2026-05-14 17:20:46 +0100

    fixing destroy pipeline so external network load balancer can get deleted

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -130,6 +130,14 @@ resource "aws_vpc_endpoint" "s3" {
   })
 }
 
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-default-sg-locked"
+  })
+}
+
 
 
 
