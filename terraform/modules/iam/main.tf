8ef8509cbca045296d9e1d3edcefd13f149f4740
rtayyabaws <rtayyabaws@gmail.com>
2026-07-23 11:14:30 +0000

    Fix GitHub Actions OIDC trust policy

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -97,7 +97,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:rtayyabaws/EKS-SRE-Platform:*"
+            "token.actions.githubusercontent.com:sub" = "repo:rtayyabaws@97921411/EKS-SRE-Platform:*"
           }
         }
       }
